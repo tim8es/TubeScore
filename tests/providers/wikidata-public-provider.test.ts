e2ba5a8c3db1595ad56b652e4de0e9d13c8f7222
tim8es <108188608+tim8es@users.noreply.github.com>
2026-09-10 13:16:56 +0300

    test: prefer ranked Wikidata external IDs

--- tests/providers/wikidata-public-provider.test.ts
+++ tests/providers/wikidata-public-provider.test.ts
@@ -120,7 +120,10 @@ describe('Wikidata public providers', () => {
             Q109228991: {
               id: 'Q109228991',
               claims: {
-                P345: [{ rank: 'normal', mainsnak: { datavalue: { value: 'tt15239678' } } }],
+                P345: [
+                  { rank: 'normal', mainsnak: { datavalue: { value: 'tt0000000' } } },
+                  { rank: 'preferred', mainsnak: { datavalue: { value: 'tt15239678' } } }
+                ],
                 P1258: [{ rank: 'normal', mainsnak: { datavalue: { value: 'm/dune_part_two' } } }],
                 P1712: [{ rank: 'normal', mainsnak: { datavalue: { value: 'movie/dune-part-two' } } }],
                 P2603: [{ rank: 'normal', mainsnak: { datavalue: { value: '1234567' } } }],
